1ca1f89bb9f791775f0c865ddfb767fe7fa6934f
Artem000L <mctema007@bk.ru>
2024-12-11 13:43:54 +0700

    add dataset

--- titanic.py
+++ titanic.py
@@ -5,5 +5,11 @@ from catboost.datasets import titanic
 titanic_df, _ = titanic()
 
 titanic_dataset = titanic_df.copy()
-titanic_dataset.to_csv("titanic_dataset.csv", index=False)
 
+# titanic_dataset.to_csv("titanic_dataset.csv", index=False)
+
+# titanic_dataset = titanic_dataset[['Pclass', 'Sex', 'Age']]
+# titanic_dataset.to_csv("titanic_dataset_cut.csv", index=False)
+
+titanic_dataset['Age'].fillna(titanic_dataset['Age'].mean(), inplace=True)
+titanic_dataset.to_csv("titanic_dataset_notnanage.csv", index=False)
