475929171b4ceacc19ef245b64236eadd00abe44
Artem000L <mctema007@bk.ru>
2024-12-11 13:03:55 +0700

    first commit with dataset

---
+++ titanic.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from catboost.datasets import titanic
+
+
+titanic_df, _ = titanic()
+
+titanic_dataset = titanic_df.copy()
+titanic_dataset.to_csv("titanic_dataset.csv", index=False)
